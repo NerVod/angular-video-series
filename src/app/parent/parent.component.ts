8cc59295f0b5b6c85f5a9edc9b74f319e48ab785
NerVod <benjamin.jeannerot@gmail.com>
2022-10-24 15:41:44 +0200

    styles binding

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  // templateUrl: './parent.component.html',
+  // styleUrls: ['./parent.component.scss']
+  template: `
+    <div [style]="{
+      width: '200px',
+      height: '200px',
+      backgroundColor: 'pink'
+    }">Is on Sale</div>
+
+  `
 })
 export class ParentComponent implements OnInit {
 
-  itemImageUrl:string = '../assets/pills.jpg'
-
-  isUnchanged:boolean = false
+  onSale = true
+  pink = "pink"
 
   constructor() { }
 
@@ -17,6 +24,6 @@ export class ParentComponent implements OnInit {
   }
 
 
-
+ 
 
 }
