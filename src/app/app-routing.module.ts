ab996373b868310a580f4c06aab1c27134dbf2d9
NerVod <benjamin.jeannerot@gmail.com>
2022-10-28 10:45:14 +0200

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ChildAComponent } from './child-a/child-a.component';
 import { ChildBComponent } from './child-b/child-b.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { YourGuardGuard } from './your-guard.guard';
 
 const routes: Routes = [
   {
-    path: 'first-component/:id', component: FirstComponent,
+    path: 'first-component/:id', component: FirstComponent, canActivate: [YourGuardGuard],
     children: [
       {path: 'child-a', component: ChildAComponent},
       {path: 'child-b', component: ChildBComponent},
